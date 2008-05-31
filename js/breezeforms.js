5824079046a4574aa4b7f6bb99a1603e815df3eb
Stefan Rusterholz <stefan@Schrulle.local>
2008-05-31 02:29:01 +0200

    more js

--- js/breezeforms.js
+++ js/breezeforms.js
@@ -16,6 +16,74 @@ Object.prototype.each = function(yield) {
 	return(this);
 };
 
+function Field(form, name, description) {
+	var field          = this;
+	this._form         = form;
+	this._name         = name;
+	this._errors       = new Array();
+	this._available    = false;
+	this._node         = $('[@name="'+form.name()+'.'+name+'"]');
+	this._expects      = description.expects;
+	this._validates_if = description.validates_if;
+
+	this.form         = function() { return(this._form); };
+	this.name         = function() { return(this._name); };
+	this.errors       = function() { return(this._errors); };
+	this.available    = function() { return(this._available); };
+	this.node         = function() { return(this._node); };
+	this.expects      = function() { return(this._expects); };
+	this.validates_if = function() { return(this._validates_if); };
+	this.valid        = function() {
+		return(this._available && this._errors.empty());
+	};
+	this.erroneous    = function() {
+		return(this._available && !(this._errors.empty()));
+	};
+	this.validate     = function() {
+	};
+	this.set_value    = function(value) {
+		this._value = value;
+	};
+	
+	this._node.change(function() {
+		if (field._add_class) {
+			field._node.addClass(field._add_class);
+		}
+	});
+}
+
+function Form(name, fields) {
+	var form        = this;
+
+	this._name      = name;
+	this._fields    = new Array()
+	this._errors    = new Array();
+	this._available = false;
+
+	this.name       = function() { return(this._name); };
+	this.fields     = function() { return(this._fields); };
+	this.errors     = function() { return(this._errors); };
+	this.available  = function() { return(this._available); };
+	this.valid      = function() {
+		return(this._available && this._errors.empty());
+	};
+	this.erroneous  = function() {
+		return(this._available && !(this._errors.empty()));
+	};
+	this.field      = function(name) {
+		return(this._fields[name]);
+	};
+
+	fields.each(function(name, description) {
+		form._fields[name] = new Field(form, name, description);
+	});
+};
+
+Adaptors = {
+	"String": function(value) { return(value); },
+	"Integer": function(value) { return(parseInt(value)); },
+	"Float": function(value) { return(parseFloat(value)); },
+};
 
 Validators = {
 	"String": {
@@ -57,6 +125,52 @@ Validators = {
 		},
 		"none_of": function(value, choices) {
 			return(false);
+		}
+	},
+	"Integer": {
+		"min": function(value, min) {
+			return(value >= min);
+		},
+		"min_exclusive": function(value, min) {
+			return(value > min);
+		},
+		"max": function(value, max) {
+			return(value <= max);
+		},
+		"max_exclusive": function(value, max) {
+			return(value < max);
+		},
+		"natural": function(value) {
+			return(value >= 0);
+		},
+		"positive": function(value) {
+			return(value > 0);
+		},
+		"negative": function(value) {
+			value >= 0
+		}
+	},
+	"Float": {
+		"min": function(value, min) {
+			return(value >= min);
+		},
+		"min_exclusive": function(value, min) {
+			return(value > min);
+		},
+		"max": function(value, max) {
+			return(value <= max);
+		},
+		"max_exclusive": function(value, max) {
+			return(value < max);
+		},
+		"natural": function(value) {
+			return(value >= 0);
+		},
+		"positive": function(value) {
+			return(value > 0);
+		},
+		"negative": function(value) {
+			value >= 0
 		}
 	}
 };
